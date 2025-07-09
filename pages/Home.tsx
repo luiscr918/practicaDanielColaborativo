aa91bd5ac524132f82cfdb842d49ae5452bafb26
luiscr918 <lc9107040@gmail.com>
2025-07-09 12:26:17 -0500

    conflictos

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -10,6 +10,8 @@ export const Home = () => {
       <HeroSectionHome />
       <h1>NOMBRE</h1>
     <Saludo nombre="luis"/>
+    <p>estoy actualizando daniel esto va a generar conflictos</p>
+    <p>no te asustes</p>
     <Link to={'/carrusel'} >ir carrusel</Link>
     <p>hoklkjlkfdsjhfd</p>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo enim distinctio consequatur excepturi dolore iusto, assumenda tempora ipsam magnam dolorum, vero repudiandae nam eaque optio nemo eligendi exercitationem. Consequuntur, voluptatem?</p>
